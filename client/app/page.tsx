b54162587450d88d9a164c94a2b7bcd5684f7edb
rohitk131 <rohitkumar13103@gmail.com>
2025-11-08 22:48:56 +0530

    setup backend and separate for both client and server

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
           <button className="group relative m-1 inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-lg border-b-2 border-l-2 border-r-2 border-neutral-700 bg-gradient-to-tr from-neutral-900 to-neutral-800 px-4 py-2 text-white shadow-lg transition duration-100 ease-in-out active:translate-y-0.5 active:border-neutral-600 active:shadow-none">
   <IconMicrophone stroke={2} />
   <span className="absolute h-0 w-0 rounded-full bg-white opacity-10 transition-all duration-300 ease-out group-hover:h-32 group-hover:w-32"></span>
-  <span className="relative font-medium">Start Recording</span>
+  <span className="relative font-medium text-lg">Start Recording</span>
 </button>
 
 
