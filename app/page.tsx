b9bc2182b98f9780441494ab3b2706ec061d2a21
rohitk131 <rohitkumar13103@gmail.com>
2025-11-08 22:33:48 +0530

    added textbox and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,56 @@
 import DarkVeil from "@/components/DarkVeil";
 import Navbar from "@/components/Navbar";
-
+import { IconMicrophone } from '@tabler/icons-react';
+import BlurText from "@/components/BlurText";
+import DecryptedText from "@/components/DecryptedText";
 export default function Home() {
   return (
-    <div className="relative h-screen w-full overflow-hidden">
-     
+    <div className="relative h-screen w-full overflow-hidden text-white">
+
       <DarkVeil />
 
-      
+
       <div className="absolute inset-0 z-10 flex flex-col">
-        
+
         <Navbar />
 
-        
-        <div className="flex flex-1 flex-col items-center justify-center text-center text-white px-4">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">
-            Turn your voice into text, instantly.
-          </h1>
-          <p className="text-lg text-gray-300 max-w-2xl">
-            You can add buttons, hero text, or components here later.
-          </p>
+
+        <div className="flex flex-1 flex-col items-center justify-center px-4 text-center">
+
+          <BlurText
+            text=" Turn your voice into text, instantly."
+            delay={150}
+            animateBy="words"
+            direction="top"
+            className="text-6xl font-medium mb-2"
+          />
+
+
+          <DecryptedText
+            text="Start Speaking. Stop Typing."
+            animateOn="view"
+            revealDirection="start"
+            speed={80}
+            maxIterations={30}
+            className=""
+
+          />
+
+
+          <div className="w-full max-w-2xl h-1/2 bg-black/40 border border-gray-800 rounded-lg flex mb-6 mt-6 p-4">
+            <p className="text-gray-400">Your text will appear here...</p>
+          </div>
+
+
+          <div className="flex flex-row gap-4">
+          <button className="group relative m-1 inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-lg border-b-2 border-l-2 border-r-2 border-neutral-700 bg-gradient-to-tr from-neutral-900 to-neutral-800 px-4 py-2 text-white shadow-lg transition duration-100 ease-in-out active:translate-y-0.5 active:border-neutral-600 active:shadow-none">
+  <IconMicrophone stroke={2} />
+  <span className="absolute h-0 w-0 rounded-full bg-white opacity-10 transition-all duration-300 ease-out group-hover:h-32 group-hover:w-32"></span>
+  <span className="relative font-medium">Start Recording</span>
+</button>
+
+
+          </div>
         </div>
       </div>
     </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,19 @@
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex items-center justify-between px-6 py-4 bg-transparent">
-      <h1
-        className={`text-3xl font-semibold text-white tracking-wide`}
-      >
-        Whispr
-      </h1>
+    <nav className="w-3/4 mx-auto flex items-center justify-between px-6 py-4 bg-transparent  ">
+      <div className="flex flex-row gap-2 items-center">
 
-       <div className="flex items-center gap-4">
+        <img src="/icon.svg" alt="icon" className="w-7 h-7" />
+
+        <h1
+          className={`text-3xl font-semibold text-white tracking-wide`}
+        >
+          Whispr
+        </h1>
+      </div>
+
+      <div className="flex items-center gap-4">
         <span className="text-zinc-400 text-sm">No signups. No limits.</span>
       </div>
     </nav>
